714b2f8d5edb725018dd2065efd45bd9020f7660
Bebop <nicolas.bibboperez@gmail.com>
2023-01-13 17:46:56 -0300

    Enemy death effect

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_effect():
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
+		var grass_effect = GrassEffect.instance()
+		get_parent().add_child(grass_effect)
+		grass_effect.global_position = global_position
 
 func _on_Hurtbox_area_entered(_area):
 	create_grass_effect()
