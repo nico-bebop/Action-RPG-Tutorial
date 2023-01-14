c25d02eb68bebceccc59002bd9b718bee5243c73
Bebop <nicolas.bibboperez@gmail.com>
2023-01-13 23:47:01 -0300

    Enemy AI

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,13 +2,44 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
-var knockback = Vector2.ZERO
+export var acceleration = 300
+export var max_speed = 50
+export var friction = 200
 
+enum {IDLE, WANDER, CHASE}
+
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO
+var state = IDLE
+
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var player_detection_zone = $PlayerDetectionZone
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = player_detection_zone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+	
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if player_detection_zone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
